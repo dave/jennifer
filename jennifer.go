6d631a0a3742a756d4dbc72396f18d0f334057e9
David Brophy <dave@brophy.uk>
2017-02-23 18:13:41 +0100

    Doc comment

--- jennifer.go
+++ jennifer.go
@@ -2,4 +2,5 @@
 //go:generate genjen
 //go:generate go get github.com/davelondon/rebecca/cmd/becca
 //go:generate becca -package=github.com/davelondon/jennifer/jen
+// Package jennifer is a code generator for Go
 package jennifer
