e351fbc941b48d4f2f81940bc6c083fd1ef79684
David Brophy <dave@brophy.uk>
2017-02-23 19:34:20 +0100

    Moved generate commands

--- jennifer.go
+++ jennifer.go
@@ -1,6 +1,7 @@
+// Package jennifer is a code generator for Go
+package jennifer
+
 //go:generate go get github.com/davelondon/jennifer/genjen
 //go:generate genjen
 //go:generate go get github.com/davelondon/rebecca/cmd/becca
 //go:generate becca -package=github.com/davelondon/jennifer/jen
-// Package jennifer is a code generator for Go
-package jennifer
