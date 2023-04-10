ff106b29ffde6fd2c49d7180f197258748285f0d
Dave Brophy <dave@brophy.uk>
2023-04-10 11:51:18 +0700

    Fixed syntax of generate commands

--- jennifer.go
+++ jennifer.go
@@ -1,9 +1,9 @@
 // Package jennifer is a code generator for Go
 package jennifer
 
-//go:generate go install github.com/dave/jennifer/genjen
+//go:generate go install github.com/dave/jennifer/genjen@latest
 //go:generate genjen
-//go:generate go install github.com/dave/jennifer/gennames
+//go:generate go install github.com/dave/jennifer/gennames@latest
 //go:generate gennames -output "jen/hints.go" -package "jen" -name "standardLibraryHints" -standard -novendor -path "./..."
-//go:generate go install github.com/dave/rebecca/cmd/becca
+//go:generate go install github.com/dave/rebecca/cmd/becca@latest
 //go:generate becca -package=github.com/dave/jennifer/jen
